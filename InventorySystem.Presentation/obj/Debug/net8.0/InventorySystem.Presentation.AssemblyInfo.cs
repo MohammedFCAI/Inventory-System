a831619575f91cb3965bcddfda3f6e4a2b4c3541
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2024-10-16 10:16:59 +0300

    update debug settings

--- InventorySystem.Presentation/obj/Debug/net8.0/InventorySystem.Presentation.AssemblyInfo.cs
+++ InventorySystem.Presentation/obj/Debug/net8.0/InventorySystem.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySystem.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674556c4608c9306e7c6ee24b57fc96106ff5495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcfeb262ca66934b0a68b592d8699372bc9f4243")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventorySystem.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventorySystem.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
